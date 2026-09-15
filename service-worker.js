51442f88eb02c350acef1ccc07b0425d6bfcbc86
rakdim04 <rakeshkoul.hr@gmail.com>
2026-09-15 14:34:06 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,59 +1,124 @@
-const CACHE_NAME = "my-personal-diary-v3";
+const CACHE_NAME = "my-personal-diary-v5";
 
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+    "./",
+    "./index.html",
+    "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
-  self.skipWaiting();
 
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-  );
+/* =====================================================
+   INSTALL
+   ===================================================== */
+
+self.addEventListener("install", event => {
+
+    self.skipWaiting();
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => {
+
+                return cache.addAll(ASSETS);
+
+            })
+
+    );
+
 });
+
+
+/* =====================================================
+   ACTIVATE
+   ===================================================== */
 
 self.addEventListener("activate", event => {
 
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
+    event.waitUntil(
+
+        caches.keys()
+            .then(keys => {
+
+                return Promise.all(
+
+                    keys
+                        .filter(key => key !== CACHE_NAME)
+                        .map(key => caches.delete(key))
+
+                );
+
+            })
+            .then(() => {
+
+                return self.clients.claim();
+
+            })
+
+    );
 
 });
 
+
+/* =====================================================
+   FETCH
+   ===================================================== */
+
 self.addEventListener("fetch", event => {
 
-  event.respondWith(
+    if (event.request.method !== "GET") {
+        return;
+    }
 
-    fetch(event.request)
 
-      .then(response => {
+    event.respondWith(
 
-        const copy =
-          response.clone();
+        fetch(event.request)
 
-        caches
-          .open(CACHE_NAME)
-          .then(cache => {
-            cache.put(event.request, copy);
-          });
+            .then(response => {
 
-        return response;
+                const copy =
+                    response.clone();
 
-      })
 
-      .catch(() =>
-        caches.match(event.request)
-      )
+                caches.open(CACHE_NAME)
+                    .then(cache => {
 
-  );
+                        cache.put(
+                            event.request,
+                            copy
+                        );
+
+                    });
+
+
+                return response;
+
+            })
+
+            .catch(() => {
+
+                return caches.match(
+                    event.request
+                )
+
+                .then(cachedResponse => {
+
+                    if (cachedResponse) {
+
+                        return cachedResponse;
+
+                    }
+
+
+                    return caches.match(
+                        "./index.html"
+                    );
+
+                });
+
+            })
+
+    );
 
 });
